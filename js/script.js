d93f6423901ecf69403fd88488fac237bbf24def
Naumenko <maks.naymenko@gmail.com>
2021-01-09 22:05:13 +0200

    Added hamburger for mobile version

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger"),
+    nav = document.querySelector(".nav"),
+    linkItem = document.querySelectorAll(".nav__link"),
+    closeElem = document.querySelector(".nav__close");
+hamburger.addEventListener("click", () => {
+    nav.classList.add("active");
+});
+closeElem.addEventListener("click", () => {
+    nav.classList.remove("active");
+});
+linkItem.forEach((item) => {
+    item.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+});
